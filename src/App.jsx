85f186763993b1bdeccd2d818cdce6465fb9833a
amanmavai <amanmavai@gmail.com>
2023-12-04 12:50:06 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl text-green-400">Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
